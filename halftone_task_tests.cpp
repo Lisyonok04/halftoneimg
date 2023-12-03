3166cd57f7eabae98d3c4d0a5a4226f21deab95a
Mary <marybelka04@gmail.com>
2023-12-03 22:25:08 +0400

    Update halftone_task_tests.cpp

--- halftone_task_tests.cpp
+++ halftone_task_tests.cpp
@@ -3,19 +3,19 @@
 #include <stdexcept>
 #include <iostream>
 
-
 using namespace std;
 
-TEST(ImgTests, Constructor) {
-    HalftoneImg<bool> h1(2, 2, false);
-    h1(0, 0) = 0; h1(0, 1) = 1; h1(1, 0) = 1; h1(1, 1) = 0;
-    bool* data = new bool[4] {0, 1, 1, 0 };
-    HalftoneImg<bool> h2(2, 2, data);
-    EXPECT_TRUE(h1 == h2);
-    EXPECT_FALSE(h1 != h2);
 
-    HalftoneImg<bool> h3(2, 2, false);
-    h3(0, 0) = 0; h3(0, 1) = 1; h3(1, 0) = 1; h3(1, 1) = 0;
-    HalftoneImg<bool> h4(h3);
-    EXPECT_EQ(h3, h4);
+TEST(HalftoneImageTests, SwapCheck) {
+    short* data1 = new short[4] {1, 2, 3, 4};
+    HalftoneImg<short> h1(2, 2, data1);
+    short* data2 = new short[4] {5, 6, 7, 8};
+    HalftoneImg<short> h2(2, 2, data2);
+    cout << h1;
+    cout << h2;
+    cout << endl;
+    swap(h1, h2);
+    cout << h1;
+    cout << h2;
 }
+
